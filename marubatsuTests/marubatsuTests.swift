fb071f6ec0b03142aef6d491ff48f4dd3e79dea9
Chia-Wei <chang.ch23@gmail.com>
2026-05-15 23:56:02 +0900

    Initial Commit

---
+++ marubatsuTests/marubatsuTests.swift
@@ -0,0 +1,18 @@
+//
+//  marubatsuTests.swift
+//  marubatsuTests
+//
+//  Created by chang chiawei on 2026/05/15.
+//
+
+import Testing
+
+struct marubatsuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
